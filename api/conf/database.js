2eb4e482f9e05f174a60d6b8c40e8df238c80ba5
epikmyk <mgmorales1989@gmail.com>
2021-04-15 21:43:34 -0700

    updated database file

--- api/conf/database.js
+++ api/conf/database.js
@@ -1,9 +1,18 @@
 const mysql = require('mysql2');
 
+/*
 const pool = mysql.createPool({
     host: "localhost",
     user: "mike",
     password: "mTlY!yzW@oximuly",
+    database: "gamerspacedb",
+    debug: false
+});*/
+
+const pool = mysql.createPool({
+    host: "165.227.213.187",
+    user: "gamerspaceadmin",
+    password: "1u6#iLey9V9gihqj",
     database: "gamerspacedb",
     debug: false
 });
