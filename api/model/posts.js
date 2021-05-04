f80d8495aa2a752104a0a328b70f6127ac62ac85
epikmyk <mgmorales1989@gmail.com>
2021-05-03 22:20:11 -0700

    fixed sql syntax error in getUserPostsAndFriendsPosts api

--- api/model/posts.js
+++ api/model/posts.js
@@ -18,11 +18,11 @@ const PostModal = {
         let baseSQL = 'SELECT p.post_id, p.post, p.created, p.user_poster_id, p.user_receiver_id, u.username\
         FROM posts p \
         JOIN friends f on p.user_poster_id=f.user_id or p.user_poster_id=f.friend_id \
-        JOIN users u on p.user_poster_id=u.user_id \
-        WHERE f.user_id = ? or f.friend_id = ? \
+        JOIN users u on p.user_poster_id=u.user_id\
+        WHERE f.user_id=? OR f.friend_id=?\
         ORDER BY p.created DESC';
 
-        return db.query(baseSQL, [_id])
+        return db.query(baseSQL, [_id, _id])
     }
 }
 
