07b298340a8e1479258780e78fd6aa2d9ee71bb8
epikmyk <mgmorales1989@gmail.com>
2021-05-12 02:39:45 -0700

    fixed error where user was seeing posts from pending friends requests

--- api/model/posts.js
+++ api/model/posts.js
@@ -33,14 +33,14 @@ const PostModal = {
         WHERE f.user_id=? AND p.user_receiver_id=? AND f.status=1\
         ORDER BY p.created DESC';*/
 
-        let baseSQL = 'WITH UserFriends AS (SELECT DISTINCT f.user_id, u.username\
+        let baseSQL = 'WITH UserFriends AS (SELECT DISTINCT f.user_id, u.username, f.status\
             FROM friends f \
             JOIN users u on f.user_id=u.user_id\
             WHERE f.user_id = ? or friend_id = ?)\
             (SELECT p.post_id, p.post, p.created, p.user_poster_id, p.user_receiver_id, u.username\
             FROM posts p \
             JOIN UserFriends u on p.user_poster_id=u.user_id \
-            WHERE p.user_receiver_id = p.user_poster_id \
+            WHERE p.user_receiver_id = p.user_poster_id AND u.status = 1\
             ORDER BY p.created DESC)';
 
         return db.query(baseSQL, [_id, _id])
