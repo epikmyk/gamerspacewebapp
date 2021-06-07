0f44b2d3e02361e6fdf982e18643410f24e36875
epikmyk <mgmorales1989@gmail.com>
2021-06-06 21:14:50 -0700

    updated posts to return image if post has one

--- api/model/posts.js
+++ api/model/posts.js
@@ -13,11 +13,11 @@ const PostModal = {
         WHERE u.username=? \
         ORDER BY p.created DESC';*/
 
-        let baseSQL = 'WITH UserPosts AS (SELECT p.post_id, p.post, p.created, p.user_poster_id, p.user_receiver_id, u.username\
+        let baseSQL = 'WITH UserPosts AS (SELECT p.post_id, p.post, p.created, p.user_poster_id, p.user_receiver_id, u.username, p.image\
             FROM posts p \
             JOIN users u on p.user_receiver_id=u.user_id \
             WHERE u.username=?)\
-            (SELECT p.post_id, p.post, p.created, p.user_poster_id, p.user_receiver_id, u.username\
+            (SELECT p.post_id, p.post, p.created, p.user_poster_id, p.user_receiver_id, u.username, p.image\
             FROM UserPosts p\
             JOIN users u on p.user_poster_id=u.user_id)\
             ORDER BY p.created DESC';
@@ -37,7 +37,7 @@ const PostModal = {
             FROM friends f \
             JOIN users u on f.user_id=u.user_id\
             WHERE f.user_id = ? or friend_id = ?)\
-            (SELECT p.post_id, p.post, p.created, p.user_poster_id, p.user_receiver_id, u.username\
+            (SELECT p.post_id, p.post, p.created, p.user_poster_id, p.user_receiver_id, u.username, p.image\
             FROM posts p \
             JOIN UserFriends u on p.user_poster_id=u.user_id \
             WHERE p.user_receiver_id = p.user_poster_id AND u.status = 1\
