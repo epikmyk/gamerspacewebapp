e3c0a23afdc6935d906a0582eaab99fa68f2601c
epikmyk <mgmorales1989@gmail.com>
2021-06-08 20:57:58 -0700

    updated startup to load homepage if logged in

--- client/src/components/startup/StartupPage.js
+++ client/src/components/startup/StartupPage.js
@@ -4,19 +4,28 @@ import '../startup/StartupPage.css'
 
 const StartupPage = props => {
 
+    useEffect(() => {
+        if (document.cookie.includes('cookieKey')) {
+            window.location.assign('/home');
+        }
+    })
+
     return (
         <div>
-            <div className='main'>
-                <div className="main-startup-page">
-                    <div className="navbar">
-                        <div className="title"><img src={"images/gamerspace-logo.png"}></img> Gamerspace</div>
-                        <div className="navbar-sign-in"><a href="/login">Sign In</a></div>
-                    </div>
-                    <div className="about">
-                        <Signup />
+            {!document.cookie.includes('cookieKey') ?
+                <div className='main'>
+                    <div className="main-startup-page">
+                        <div className="navbar">
+                            <div className="title"><img src={"images/gamerspace-logo.png"}></img> Gamerspace</div>
+                            <div className="navbar-sign-in"><a href="/login">Sign In</a></div>
+                        </div>
+                        <div className="about">
+                            <Signup />
+                        </div>
                     </div>
                 </div>
-            </div>
+                : null
+            }
         </div>
     )
 }
