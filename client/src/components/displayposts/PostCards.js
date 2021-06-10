de2be883367c7df5db1f4672c031e577c4f352dd
epikmyk <mgmorales1989@gmail.com>
2021-06-09 21:08:50 -0700

    updated posts to be a link

--- client/src/components/displayposts/PostCards.js
+++ client/src/components/displayposts/PostCards.js
@@ -2,9 +2,6 @@ import React, { useState, useEffect } from 'react';
 import { FaUserCircle, FaRegComment, FaRegHeart, FaHeart } from 'react-icons/fa'
 import './PostCards.css';
 
-var ReactDOMServer = require('react-dom/server');
-var HtmlToReactParser = require('html-to-react').Parser;
-
 const PostCards = props => {
     const [listOfPosts, setListOfPosts] = useState([]);
 
@@ -15,26 +12,29 @@ const PostCards = props => {
     const postCards = listOfPosts.map(post =>
 
         <div>
-                <div className="post-username">
-                    <FaUserCircle size={32} color={"#293E4A"}></FaUserCircle> {post.username}
-                </div>
-                <div className="post-container">
+            <div className="post-username">
+                <FaUserCircle size={32} color={"#293E4A"}></FaUserCircle> {post.username}
+            </div>
+            <div className="post-container">
+                <a href="#">
                     <div className="post-text">
                         {post.post}
                     </div>
                     {post.image !== ' ' ?
                         <div className="post-image"><img src={post.image} width="360"></img></div>
                         : null}
+                </a>
+
+            </div>
+            <div className="post-stats-container">
+                <div className="num-of-comments">
+                    <p><FaRegComment size={18} color={"#888888"}></FaRegComment> 0 Comments</p>
                 </div>
-                <div className="post-stats-container">
-                    <div className="num-of-comments">
-                        <p><FaRegComment size={18} color={"#888888"}></FaRegComment> 0 Comments</p>
-                    </div>
-                    <div className="num-of-likes">
-                        <p><FaRegHeart size={18} color={"#888888"}></FaRegHeart> 0 Likes</p>
-                    </div>
+                <div className="num-of-likes">
+                    <p><FaRegHeart size={18} color={"#888888"}></FaRegHeart> 0 Likes</p>
                 </div>
-                <hr className="post-seperator"></hr>
+            </div>
+            <hr className="post-seperator"></hr>
         </div>
     )
 
