08f6a0a22ed7421c6ad8add1382789481c54f269
epikmyk <mgmorales1989@gmail.com>
2021-06-11 22:41:46 -0700

    added favorite games card component to profile

--- client/src/components/profile/Profile.js
+++ client/src/components/profile/Profile.js
@@ -2,16 +2,50 @@ import React, { useState, useEffect } from 'react';
 import NavBar from '../common/NavBar';
 import '../profile/Profile.css';
 import Feed from "../profile/Feed";
+import { FaUserCircle } from 'react-icons/fa'
+import WritePost from '../home/WritePost';
+import FavoriteGamesCards from '../profile/FavoriteGamesCards';
+import { Carousel } from 'react-bootstrap';
 
 const Profile = props => {
 
+    const [user, setUser] = useState({})
+    const [username, setUsername] = useState("")
+    const wallPostUrl = "/api/posts/getRecentPostsToUser/" + username
+
+    const getUser = () => {
+        fetch('/api/users/getUser/' + username)
+            .then(res => res.json())
+            .then(res => setUser(res))
+            .catch(err => err)
+    }
+
+    const getUsernameFromUrl = () => {
+        let url = window.location.href;
+        url = url.split('/');
+        url = url.pop();
+
+        return url;
+    }
+
+    useEffect(() => {
+        setUsername(getUsernameFromUrl)
+        getUser();
+    }, [username])
+
     return (
         <div>
-            <div className='main'>
-                <NavBar />
-                <div className="main-profile">
+            <div className='main-profile-root-container'>
+                <NavBar user={user} />
+                <div className="main-profile-content-container">
+                    <div className="profile-header-container">
+                        <div className="profile-header-username"><FaUserCircle size={65} color={"#293E4A"}></FaUserCircle> {user.username}</div>
+                        <div className="favorite-games-slider-container">
+                            <FavoriteGamesCards username={username}/>
+                        </div>
+                    </div>
                     <div className="profile-feed">
-                        <Feed />
+                        <WritePost user={user} wallPostUrl={wallPostUrl} />
                     </div>
                 </div>
             </div>
