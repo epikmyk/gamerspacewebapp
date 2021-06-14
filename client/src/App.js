67d4ae7958f9b720351bc486e95af5da4a68e1ef
epikmyk <mgmorales1989@gmail.com>
2021-06-13 17:47:31 -0700

    added usercontext for user in main app component

--- client/src/App.js
+++ client/src/App.js
@@ -1,6 +1,6 @@
 import logo from './logo.svg';
 import './App.css';
-import React, { useEffect } from 'react';
+import React, { useEffect, useState, useMemo, useContext } from 'react';
 import { BrowserRouter, Route, Switch } from 'react-router-dom'
 
 import Homepage from './components/home/Homepage';
@@ -8,22 +8,37 @@ import Chat from './components/chat/Chat';
 import Profile from './components/profile/Profile';
 import LoginPage from './components/startup/LoginPage';
 import StartupPage from './components/startup/StartupPage';
+import UserContext from './components/common/UserContext';
 
 function App() {
 
+  const [user, setUser] = useState({})
+
+  const provideUser = useMemo(() => ({ user, setUser }, [user, setUser]));
+
+  const getLoggedInUser = () => {
+    fetch('/api/users/getLoggedInUser')
+      .then(res => res.json())
+      .then(res => setUser(res))
+      .catch(err => err)
+  }
+
   useEffect(() => {
     document.title = "gamerspace"
+    getLoggedInUser();
   }, []);
 
   return (
     <main>
-      <style>{'body { height: 100%; margin: 0;'}</style>
+      <style>{'body { height: 100%; margin: 0; overflow: scroll'}</style>
       <Switch>
-        <Route path='/' component={StartupPage} exact/>
-        <Route path='/home' component={Homepage} exact/>
-        <Route path='/chat' component={Chat} exact/>
-        <Route path='/profile' component={Profile} exact/>
-        <Route path='/login' component={LoginPage} exact/>
+        <UserContext.Provider value={provideUser}>
+          <Route path='/' component={StartupPage} exact />
+          <Route path='/home' component={Homepage} exact />
+          <Route path='/chat' component={Chat} exact />
+          <Route path='/profile/:username' component={Profile} exact />
+          <Route path='/login' component={LoginPage} exact />
+        </UserContext.Provider>
       </Switch>
     </main>
   )
