3f2df714216021b3a518b35efb4aa32879072619
epikmyk <mgmorales1989@gmail.com>
2021-06-30 00:15:27 -0700

    added components to chat to display users on right hand side

--- client/src/components/chat/Chat.js
+++ client/src/components/chat/Chat.js
@@ -1,27 +1,58 @@
-import React, { useState, useEffect, useRef } from 'react';
-import { Button, Form } from 'react-bootstrap';
+import React, { useState } from 'react';
+import { Form, FormControl } from 'react-bootstrap';
+import { IoMdSend } from 'react-icons/io'
 import NavBar from '../common/NavBar';
+import DisplayFriends from './DisplayFriends';
+import DisplaySearchResults from './DisplaySearchResults';
 import '../chat/Chat.css'
 
 const Chat = props => {
+
+    const [searchTerm, setSearchTerm] = useState("");
+
+    const handleSearchChange = (e) => {
+        setSearchTerm(e.target.value);
+    }
+
     return (
         <>
-            <NavBar />
             <div className="chat-container">
-                <div className="chat-content">
-                    Hello
+                <NavBar />
+                <div className="chat-content-container">
+                    <div className="chats">
+                        <div className="chats-header">Chats</div>
+                    </div>
+                    <div className="chat">
+                        <div className="chat-name-header">
+                        </div>
+                        <Form className="chat-form">
+                            <FormControl type="text" placeholder="Send a message..." className="chat-input"></FormControl>
+                            <a className="send-message-button">
+                                <IoMdSend color={"#293E4A"} size={40}></IoMdSend>
+                            </a>
+                        </Form>
+                    </div>
+                    <div className="people">
+                        <div className="people-top">
+                            <div className="people-header">People</div>
+                            <div class="people-search">
+                                <Form className="people-search-form">
+                                    <FormControl type="text" placeholder="Search Users" className="people-search-input" onChange={handleSearchChange}></FormControl>
+                                </Form>
+                            </div>
+                        </div>
+                        <div className="people-container">
+                            <div>
+                                {searchTerm === "" ?
+                                    <DisplayFriends />
+                                    : <DisplaySearchResults searchTerm={searchTerm} />}
+                            </div>
+                        </div>
+                    </div>
                 </div>
-                <div className="chat">
-                    <Form className="chat-form">
-                        <textarea className="chat-textarea" placeholder="Send a message..." maxLength="500"></textarea>
-                    </Form>
-                    {/*<Button className="send-message-button" size="sm">Chat</Button>*/}
-                </div>
-                
             </div>
         </>
     )
-
 }
 
 export default Chat;
