cb01155f7a1a9a52b59e662953a07d739a4ecc3e
epikmyk <mgmorales1989@gmail.com>
2021-07-02 11:48:00 -0700

    added component for displaying active chats

--- client/src/components/chat/Chat.js
+++ client/src/components/chat/Chat.js
@@ -1,18 +1,41 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 import { Form, FormControl } from 'react-bootstrap';
 import { IoMdSend } from 'react-icons/io'
 import NavBar from '../common/NavBar';
 import DisplayFriends from './DisplayFriends';
 import DisplaySearchResults from './DisplaySearchResults';
+import DisplayUserChats from './DisplayUserChats';
 import ChatHeader from './ChatHeader';
 import '../chat/Chat.css'
 
 const Chat = props => {
 
     const [searchTerm, setSearchTerm] = useState("");
+    const [message, setMessage] = useState("");
+    const [initializeChat, setInitializeChat] = useState(false);
 
     const handleSearchChange = (e) => {
         setSearchTerm(e.target.value);
+    }
+
+    const handleMessageChange = (e) => {
+        setMessage(e.target.value)
+    }
+
+    const sendMessage = () => {
+        const data = {
+            message: message,
+            chat_id: props.match.params.chatId
+        }
+        fetch('/api/messages/createMessage', {
+            method: 'POST',
+            headers: {
+                'Content-Type': 'application/json',
+            },
+            body: JSON.stringify(data)
+        })
+            .then(data => console.log(data))
+            .catch((err) => console.log(err))
     }
 
     return (
@@ -22,14 +45,15 @@ const Chat = props => {
                 <div className="chat-content-container">
                     <div className="chats">
                         <div className="chats-header">Chats</div>
+                        <DisplayUserChats initializeChat={initializeChat} chatId={props.match.params.chatId}/>
                     </div>
                     <div className="chat">
                         <div className="chat-name-header">
-                           <ChatHeader chatId={props.match.params.chatId}/>
+                            <ChatHeader chatId={props.match.params.chatId} />
                         </div>
                         <Form className="chat-form">
-                            <FormControl type="text" placeholder="Send a message..." className="chat-input"></FormControl>
-                            <a className="send-message-button">
+                            <FormControl type="text" placeholder="Send a message..." className="chat-input" onChange={handleMessageChange}></FormControl>
+                            <a className="send-message-button" onClick={() => {sendMessage(); setInitializeChat(true)}}>
                                 <IoMdSend color={"#293E4A"} size={40}></IoMdSend>
                             </a>
                         </Form>
---
+++ client/src/components/chat/DisplayUserChats.js
@@ -0,0 +1,60 @@
+import React, { useState, useEffect, useContext } from 'react';
+import PeopleCards from './PeopleCards';
+import UserContext from '../common/UserContext';
+import '../displayusers/UserCards.css'
+
+const DisplayUserChats = props => {
+
+    const [loggedInUser] = useContext(UserContext);
+    const [listOfUsers, setListOfUsers] = useState([])
+    const [chatActive, setChatActive] = useState(false)
+
+    const getChats = () => {
+        fetch('/api/chat/getChats')
+            .then(res => res.json())
+            .then(res => {
+                setListOfUsers(res);
+            })
+            .catch(err => err)
+    }
+
+    const checkChatStatus = () => {
+
+        console.log("active: " + chatActive)
+        if (props.initializeChat && chatActive === false) {
+            console.log("initializing chat " + props.chatId)
+
+            const data = {
+                active: 1,
+                chat_id: props.chatId
+            }
+            fetch('/api/chat/updateChatStatus', {
+                method: 'PATCH',
+                headers: {
+                    'Content-Type': 'application/json',
+                },
+                body: JSON.stringify(data)
+            })
+                .then(data => {
+                    getChats();
+                })
+                .catch((err) => {
+                    console.log(err);
+                })
+                setChatActive(true)
+        }
+    }
+
+    useEffect(() => {
+        getChats();
+        checkChatStatus();
+    }, [loggedInUser, props.initializeChat, props.chatId])
+
+    return (
+        <>
+            <PeopleCards listOfPeople={listOfUsers}/>
+        </>
+    )
+}
+
+export default DisplayUserChats;
