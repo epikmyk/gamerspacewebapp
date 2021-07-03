9fac80d5b9b58383e20c2231664092fb6ab71354
epikmyk <mgmorales1989@gmail.com>
2021-07-02 23:34:07 -0700

    updated get messages to return profile pic

--- api/model/messages.js
+++ api/model/messages.js
@@ -8,9 +8,11 @@ const MessageModel = {
         return db.execute(baseSQL, [message, user_id, chat_id])
     },
     retrieveMessages: function (chat_id) {
-        let baseSQL = 'SELECT m.message, m.created, m.user_id, m.chat_id \
+        let baseSQL = 'SELECT m.message, m.created, m.user_id, m.chat_id, u.username, u.profile_pic \
         FROM messages m\
-        JOIN chat c ON c.chat_id = m.chat_id';
+        JOIN chat c ON c.chat_id = m.chat_id\
+        JOIN users u ON u.user_id = m.user_id\
+        WHERE c.chat_id = ?';
 
         return db.query(baseSQL, [chat_id])
     }
